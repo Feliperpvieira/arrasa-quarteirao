cc6f42c32e5cf5e75c000c241c642c772853fafc
Vitor <vitorouro2009@hotmail.com>
2021-03-27 20:01:16 -0300

    V 1.0
    - O Monstro anda
    - O Monstro gira na direção da caminhada
    - Ao colidir com objetos, o Monstro não muda de direção, nem gira.
    - Ao soltar o teclado, o monstro para de andar.

---
+++ Arrasa Quarteirão/Assets/Arrasa Quarteirao/Assets/Scripts/MonstroScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonstroScript : MonoBehaviour
+{
+
+    public float speed;
+    public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        //Movimetacao
+             //O "Raw" depois de GetAxis faz com que o personagem só mexa enquanto a tecla está pressionada
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
+        movementDirection.Normalize();
+
+        //Rotacao
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
