3f2e99de73341ddcd7e5994a12ce45db7d7b2a4c
Vitor <vitorouro2009@hotmail.com>
2021-04-06 10:45:59 -0300

    AQ V7
    Adição do cronômetro

---
+++ Arrasa Quarteirao/Assets/Scripts/CronometroScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CronometroScript : MonoBehaviour
+{
+    public float totalTime;
+
+    private float minutes;
+    private float seconds;
+
+    public Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= 1 * Time.deltaTime;
+        minutes = (int)(totalTime / 60);
+        seconds = (int)(totalTime % 60);
+
+        countdownText.text = minutes.ToString() + ":" + seconds.ToString();
+
+        if (totalTime <= 0)
+        {
+            totalTime = 0;
+        }
+    }
+}
