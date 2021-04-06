a97cf5bb30ff55f33faca5e1518a4ea587eb764f
Felipe <feliperpv@hotmail.com>
2021-04-05 21:13:10 -0300

    AQ V6
    Adicionada a NavMesh
    Navegação bugada

---
+++ Arrasa Quarteirao/Assets/Scripts/InimigoController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class InimigoController : MonoBehaviour {
+
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = MonstroManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    //Desenha esfera vermelha com o tamanho do raio de busca utilizado ao selecionar o "Inimigo"
+    void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.red; 
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+}
---
+++ Arrasa Quarteirao/Assets/Scripts/MonstroManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonstroManager : MonoBehaviour
+{
+
+    #region Singleton
+
+    public static MonstroManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    public GameObject player;
+
+}
