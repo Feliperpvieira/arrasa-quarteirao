e16c3e85dff4884b633b1b07b50e5edeca0e8422
Felipe <feliperpv@hotmail.com>
2021-04-09 16:41:09 -0300

    add botao de reiniciar

---
+++ Arrasa Quarteirao/Assets/Scripts/BotaoReiniciar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotaoReiniciar : MonoBehaviour
+{
+    public void restartScene()
+    {
+        SceneManager.LoadScene("SampleScene"); //Alterar nome entre aspas para mudar qual cena o botão de reiniciar vai carregar
+    }
+}
