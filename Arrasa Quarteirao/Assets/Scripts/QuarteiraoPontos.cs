3ca30e939770df5d5bbff2a390581608fb88e90f
Felipe <feliperpv@hotmail.com>
2021-05-13 10:55:06 -0300

    add bonus quarteirao

---
+++ Arrasa Quarteirao/Assets/Scripts/QuarteiraoPontos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuarteiraoPontos : MonoBehaviour
+{
+    public int bonusQuarteiraoScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.childCount == 0)
+        {
+            Pontuacao.score = Pontuacao.score + bonusQuarteiraoScore;
+            enabled = false;
+        }
+    }
+}
