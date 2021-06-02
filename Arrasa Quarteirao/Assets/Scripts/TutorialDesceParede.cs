8073366785b4583057a4b465dd99016d68c9b211
Felipe <feliperpv@hotmail.com>
2021-06-01 21:00:45 -0300

    update tutorial

---
+++ Arrasa Quarteirao/Assets/Scripts/TutorialDesceParede.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDesceParede: MonoBehaviour
+{
+
+    public int pontuacaoParaMover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Pontuacao.score >= pontuacaoParaMover)
+        {
+            if (transform.position.y > -3)
+            {
+                transform.Translate(Vector3.down * Time.deltaTime * 3f, Space.World);
+            }
+        }
+    }
+}
