fc042f9cf4cfe678c908c544ac8d928bca035066
Felipe <feliperpv@hotmail.com>
2021-06-04 17:14:49 -0300

    Add contador de estrelas por mundo e botao 3D

--- Arrasa Quarteirao/Assets/Scripts/Menus/LevelSelector.cs
+++ Arrasa Quarteirao/Assets/Scripts/Menus/LevelSelector.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LevelSelector : MonoBehaviour
 {
-
+	public TMP_Text numeroEstrelas;
+	public string codigoDoMundo;
     public Button[] levelButtons;
+
+	private int totalEstrelasMundo;
 
     void Start ()
 	{
+		totalEstrelasMundo = 0;
+
 		int levelReached = PlayerPrefs.GetInt("levelAtual");
 
 		for (int i = 0; i < levelButtons.Length; i++)
 		{
 			if (i > levelReached)
+			{
 				levelButtons[i].interactable = false;
+			}
+			totalEstrelasMundo = totalEstrelasMundo + PlayerPrefs.GetInt(codigoDoMundo + "L" + i);
 		}
+		numeroEstrelas.text = totalEstrelasMundo.ToString();
 	}
 
     public void Select (string levelName)
