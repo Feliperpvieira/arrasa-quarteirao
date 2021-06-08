b0a48ea9e52e16f6748cf6d83624405c19a9ac90
Felipe <feliperpv@hotmail.com>
2021-06-08 17:24:17 -0300

    add barrinha dourada no botao de level

--- Arrasa Quarteirao/Assets/Scripts/Menus/LevelSelector.cs
+++ Arrasa Quarteirao/Assets/Scripts/Menus/LevelSelector.cs
@@ -11,7 +11,7 @@ public class LevelSelector : MonoBehaviour
 
 	private int totalEstrelasMundo;
 
-    void Start ()
+    void Update () //Era start, mas eu mudei pra Update pra fazer o Select() continuar pra sempre. Checar outras formas pra deixar de usar isso tudo num Update
 	{
 		totalEstrelasMundo = 0;
 		codigoDoMundo = SceneManager.GetActiveScene().name;
@@ -20,18 +20,22 @@ public class LevelSelector : MonoBehaviour
 
 		for (int i = 0; i < levelButtons.Length; i++)
 		{
+			if (i == levelReached)
+			{
+				levelButtons[i].Select();
+			}
 			if (i > levelReached)
 			{
-				levelButtons[i].interactable = false;
+				levelButtons[i].interactable = false; //Bloqueia as fases seguintes das que você ainda não chegou
 			}
-			totalEstrelasMundo = totalEstrelasMundo + PlayerPrefs.GetInt(codigoDoMundo + "L" + i);
+			totalEstrelasMundo = totalEstrelasMundo + PlayerPrefs.GetInt(codigoDoMundo + "L" + i); //Contabiliza o total de estrelas do mundo
 		}
 		numeroEstrelas.text = totalEstrelasMundo.ToString();
 	}
 
-    /*public void Select (string levelName)
+    public void SelectScene (string levelName)
     {
         SceneManager.LoadScene(levelName);
-    }*/
+    }
 
 }
