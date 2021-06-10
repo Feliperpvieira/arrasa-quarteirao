cec1b8c8df69f34b197013825264d51907b673a0
Felipe <feliperpv@hotmail.com>
2021-06-10 19:39:57 -0300

    add colisores na guarita
    -falta fazer ela subir e descer lentamente

---
+++ Arrasa Quarteirao/Assets/GuaritaRotation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuaritaRotation : MonoBehaviour
+{
+    public int tempoIntervalo;
+    float angle = 90f;
+
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+        while (true)
+        {
+            if (angle == 90f)
+            {
+                angle = -90f;
+            }
+            else if(angle == -90f)
+            {
+                angle = 90f;
+            }
+            transform.Rotate(angle, 0.0f, 0f);
+            yield return new WaitForSeconds(tempoIntervalo);
+        }
+    }
+
+}
