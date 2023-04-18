17d3b20f2e84befce92a40727a81e27856cdb326
Felipe <47862856+Feliperpvieira@users.noreply.github.com>
2023-04-17 22:52:58 -0300

    menu configuracoes add

---
+++ Arrasa Quarteirao/Assets/Scripts/Menus/AbreUrl.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class AbreUrl : MonoBehaviour
+{
+    public string url;
+
+    public void OpenURL()
+    {
+        Application.OpenURL(url);
+    }
+
+}
